903f085efbdd82783d0b74984bc46fe1e8986909
ShareAbraham <sharethomas81@gmail.com>
2024-11-11 17:49:24 -0600

    added export { default as links } from ./links; in the store/index.js file

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 export { default as home } from "./home";
 export { default as about } from "./about";
 export { default as contact } from "./contact";
-// export { default as foodMenu } from "./foodMenu";
+export { default as links } from "./links";
+//export { default as foodMenu } from "./foodMenu";
